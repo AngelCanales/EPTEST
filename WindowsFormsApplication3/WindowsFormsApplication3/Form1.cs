0fd28b89916dce9632438d066c947f80e294c4f3
Angel Canales <Angel.Canales@grupoleitz.com>
2017-09-06 16:18:16 -0600

    Inicio de el proyecto

---
+++ WindowsFormsApplication3/WindowsFormsApplication3/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using ConecxionDB.Dataset.DatasetConecxionTableAdapters;
+namespace WindowsFormsApplication3
+{
+
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            var x = new EmpleadoTableAdapter();
+            x.InsertEmpleado(textBox1.Text, textBox2.Text);
+            dataGridView1.DataSource = x.GetDataByEmpleado();
+            dataGridView1.Refresh();
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            var x = new EmpleadoTableAdapter();
+            dataGridView1.DataSource = x.GetDataByEmpleado();
+            dataGridView1.Refresh();
+
+        }
+    }
+}
